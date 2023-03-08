9a52e0c4d2eeeeaa4fc8e504babe64657cb38c09
Saad Abid <absa21kz@student.ju.se>
2023-03-08 14:55:44 +0500

    adding movement to ship

--- sketch.js
+++ sketch.js
@@ -7,12 +7,23 @@ function draw() {
   background(0);
   player.show();
   player.move();
+
+  if (keyIsDown(UP_ARROW)) {
+    player.vel.add(0, -0.1);
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    player.vel.add(0.025, 0);
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    player.vel.add(-0.025, 0);
+  }
 }
 
 function ship() {
   this.pos = createVector(width / 2, height / 2);
   this.vel = createVector(0, 0);
-  this.gravity = createVector(0, 0.5);
+  this.gravity = createVector(0, 0.05);
   this.w = 25;
   this.h = 100;
 
