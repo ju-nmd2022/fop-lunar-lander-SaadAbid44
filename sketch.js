2a9160878f1f3f1265a844834a0d7b2b54e0898c
Saad Abid <absa21kz@student.ju.se>
2023-03-08 14:44:28 +0500

    adding gravity

--- sketch.js
+++ sketch.js
@@ -6,16 +6,23 @@ function setup() {
 function draw() {
   background(0);
   player.show();
+  player.move();
 }
 
 function ship() {
   this.pos = createVector(width / 2, height / 2);
   this.vel = createVector(0, 0);
+  this.gravity = createVector(0, 0.5);
   this.w = 25;
   this.h = 100;
 
   this.show = function () {
     fill(255);
     rect(this.pos.x, this.pos.y, this.w, this.h);
+  };
+
+  this.move = function () {
+    this.vel.add(this.gravity);
+    this.pos.add(this.vel);
   };
 }
