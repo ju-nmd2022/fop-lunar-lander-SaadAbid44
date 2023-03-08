7f71f22aa40c12cf1d5861eae0eca66671470d63
Saad Abid <absa21kz@student.ju.se>
2023-03-08 15:24:57 +0500

    adding a landing sapce

--- sketch.js
+++ sketch.js
@@ -1,28 +1,33 @@
+let player;
+let landing;
+
 function setup() {
   createCanvas(800, 600);
   player = new ship();
+  landing = new space();
 }
 
 function draw() {
   background(0);
   player.show();
   player.move();
+  landing.show();
 
   if (keyIsDown(UP_ARROW)) {
-    player.vel.add(0, -0.1);
+    player.vel.y -= 0.1;
   }
 
   if (keyIsDown(RIGHT_ARROW)) {
-    player.vel.add(0.025, 0);
+    player.vel.x += 0.025;
   }
   if (keyIsDown(LEFT_ARROW)) {
-    player.vel.add(-0.025, 0);
+    player.vel.x -= 0.025;
   }
 }
 
 function ship() {
   this.pos = createVector(width / 2, height / 2);
-  this.vel = createVector(0, 0);
+  this.vel = createVector(0.5, 0);
   this.gravity = createVector(0, 0.05);
   this.w = 25;
   this.h = 100;
@@ -35,5 +40,17 @@ function ship() {
   this.move = function () {
     this.vel.add(this.gravity);
     this.pos.add(this.vel);
+  };
+}
+
+function space() {
+  this.x = width / 2;
+  this.y = height - 40;
+  this.w = 180;
+  this.h = 20;
+
+  this.show = function () {
+    fill(255);
+    rect(this.x, this.y, this.w, this.h);
   };
 }
